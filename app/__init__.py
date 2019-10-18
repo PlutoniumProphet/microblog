ba0aa0de6f6af3d215392b6bd8f7c3ce0044a87d
grahamkesley <grahamkesley@gmail.com>
2019-10-18 18:53:47 +0100

    Login and Logout user methods created.

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_login import LoginManager
 
 
 # defined as an instance of Flask class making it a member of app package
@@ -9,7 +10,7 @@ app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
-
+login = LoginManager(app)
 
 # imported at the bottom as a work around for circular imports
 from app import routes, models

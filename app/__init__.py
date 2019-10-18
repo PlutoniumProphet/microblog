df0124e238e1677e4169961efad5f17cdda33fc6
grahamkesley <grahamkesley@gmail.com>
2019-10-18 15:51:48 +0100

    Login form, route, menu and error messaging added

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,9 @@
 from flask import Flask
+from config import Config
 
 # defined as an instance of Flask class making it a member of app package
 app = Flask(__name__)
+app.config.from_object(Config)
 
 # imported at the bottom as a work around for circular imports
 from app import routes
--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,8 @@
 """This is the routing module for flask application"""
-from flask import render_template
+from flask import flash, redirect, render_template
 from app import app
+from app.forms import LoginForm
+
 
 @app.route('/')
 @app.route('/index')
@@ -28,3 +30,13 @@ def index():
         }
     ]
     return render_template('index.html', title='Home', user=user, posts=posts)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect('/index')
+    return render_template('login.html', title='Sign In', form=form)

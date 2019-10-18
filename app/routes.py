97c4f62a772d5fd87b3880b44bcd87fc6283aa04
grahamkesley <grahamkesley@gmail.com>
2019-10-18 17:11:27 +0100

    Added Flask's url_for for internal direction

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,5 @@
 """This is the routing module for flask application"""
-from flask import flash, redirect, render_template
+from flask import flash, redirect, render_template, url_for
 from app import app
 from app.forms import LoginForm
 
@@ -38,5 +38,5 @@ def login():
     if form.validate_on_submit():
         flash('Login requested for user {}, remember_me={}'.format(
             form.username.data, form.remember_me.data))
-        return redirect('/index')
+        return redirect(url_for('index'))
     return render_template('login.html', title='Sign In', form=form)

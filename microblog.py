2a182b55eae078afa5e0f184e25dd520957fbfd3
grahamkesley <grahamkesley@gmail.com>
2019-10-18 18:02:59 +0100

    Flask shell context managed in microblog.py

--- microblog.py
+++ microblog.py
@@ -1,6 +1,11 @@
 """A microblog tutorial from https://blog.miguelgrinberg.com/"""
 # from app package import app (instance of Flask class)
-from app import app
+from app import app, db
+from app.models import User, Post
+
+@app.shell_context_processor
+def make_shell_context():
+    return {'db': db, 'User': User, 'Post': Post}
 
 # FLASK_APP=microblog.py doesn't persist across terminal session
 # so install python-dotenv and add to .flaskenv at top level file

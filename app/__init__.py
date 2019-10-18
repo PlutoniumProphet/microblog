42a0cbdf67b632ff6cdee9ceef7a5df7cd12c28e
grahamkesley <grahamkesley@gmail.com>
2019-10-18 17:20:40 +0100

    Added SQLAlchemy to config

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,15 @@
 from flask import Flask
 from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
 
 # defined as an instance of Flask class making it a member of app package
 app = Flask(__name__)
 app.config.from_object(Config)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
 
 # imported at the bottom as a work around for circular imports
-from app import routes
+from app import routes, models

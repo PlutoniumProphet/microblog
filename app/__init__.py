86584782e788e386de270cc4c11472805e1e0fe5
grahamkesley <grahamkesley@gmail.com>
2019-10-24 18:57:06 +0100

    Email support added and tokenised password reset included.

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,7 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
+from flask_mail import Mail
 import logging
 from logging.handlers import SMTPHandler, RotatingFileHandler
 import os
@@ -15,6 +16,7 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
+mail = Mail(app)
 
 
 # if not in debug-mode and when email server exists in the configuration.

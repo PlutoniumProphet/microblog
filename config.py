96f3b6c5935baf8bbeb475a22849779839ddc16e
grahamkesley <grahamkesley@gmail.com>
2019-11-05 17:28:06 +0000

    Added'de'to languages in config as missed off in refactorign.

--- config.py
+++ config.py
@@ -15,7 +15,7 @@ class Config(object):
     MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-    ADMINS = ['grahamkesley@gmail.com']
-    LANGUAGES = ['en', 'es']
+    ADMINS = ['ashtreeitsolutions@gmail.com']
+    LANGUAGES = ['en', 'es', 'de']
     MS_TRANSLATOR_KEY = os.environ.get('MS_TRANSLATOR_KEY')
     POSTS_PER_PAGE = 20
